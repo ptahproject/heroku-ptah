cda40c3b01bfb4c5ee59569419a04497a2f5398c
runyaga <runyaga@alan-dev.(none)>
2012-05-08 18:32:42 -0500

    update

--- urbsly/views.py
+++ urbsly/views.py
@@ -8,6 +8,21 @@ log = logging.getLogger(__name__)
 @view_config(renderer='urbsly:templates/homepage.pt',
              route_name='home')
 
+ptah.layout.register('page', renderer='urbsly:templates/layoutpage.pt', use_global_views=True)
+
+@ptah.layout('workspace', 
+             parent="page", renderer="urbsly:templates/layoutworkspace.pt",
+             use_global_views=True)
+
+class LayoutWorkspace(ptah.View):
+
+    def update(self):
+        self.root = getattr(self.request, 'root', None)
+        self.user = ptah.auth_service.get_current_principal()
+        self.isAnon = self.user is None
+        self.ptahManager = ptah.manage.check_access(
+            ptah.auth_service.get_userid(), self.request)
+
 class HomepageView(object):
 
     def __init__(self, request):
