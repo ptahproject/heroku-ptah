4f30f2ce0255961942219d1cf439951f192c2a5b
runyaga <runyaga@alan-dev.(none)>
2012-05-08 17:25:49 -0500

    chnages y

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent runapp
+web: ./run
+#gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent runapp
--- runapp.py
+++ runapp.py
@@ -1,10 +1,11 @@
 import os
 
 from paste.deploy import loadapp
-from waitress import serve
+from gevent.wsgi import WSGIServer
+#from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:settings.ini', relative_to='.')
-    
-    serve(app, host='0.0.0.0', port=port)
+    WSGIServer(('0.0.0.0', int(os.environ.get("PORT", 5000)), app).server_forever()
+    #serve(app, host='0.0.0.0', port=port)
